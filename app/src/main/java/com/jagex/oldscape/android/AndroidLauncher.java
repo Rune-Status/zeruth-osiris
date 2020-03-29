5b9da35cd8fe788d227ec49637d1b47cfb164c21
TheRealNull <TheRealNull@gmail.com>
2020-03-29 00:47:24 -0400

    OSirsi: cleanup, no lib, no apk

---
+++ app/src/main/java/com/jagex/oldscape/android/AndroidLauncher.java
@@ -0,0 +1,11 @@
+package com.jagex.oldscape.android;
+
+import android.app.NativeActivity;
+
+
+/*
+    This is a stub class that allows us to interface the target activity.
+ */
+public class AndroidLauncher extends NativeActivity {
+
+}
