3c93c4b1e656d9b26698bb5ba2ebf04be3c768cb
TheRealNull <TheRealNull@gmail.com>
2020-03-29 15:46:42 -0400

    OSiris: fix headers

--- app/src/main/java/com/osiris/api/model/RSField.java
+++ app/src/main/java/com/osiris/api/model/RSField.java
@@ -1,5 +1,5 @@
 /*
-Copyright $today.year Null(zeruth)
+Copyright 2020 Null(zeruth)
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
